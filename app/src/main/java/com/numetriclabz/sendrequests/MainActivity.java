e6863eb7fe94326d12c2f722b40d3f14d8c5a12a
Karim <karim_ali_ma@hotmail.com>
2016-06-07 00:44:51 +0300

    Modified the main display view for application
    -added the json parser from the API call of GetAll
    -dynamically add the buttons on the layout after the parsing
    -set the layout to be a scroller view for when buttons overflow off the page
    -UI mods are applied on a separate UI thread which doesn't freeze the activity
    -comments to be added on next commit

--- app/src/main/java/com/numetriclabz/sendrequests/MainActivity.java
+++ app/src/main/java/com/numetriclabz/sendrequests/MainActivity.java
@@ -14,6 +14,7 @@ import javax.net.ssl.HttpsURLConnection;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,44 +22,113 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
-
+    public static Activity activity;
     private ProgressDialog progress;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity = this;
+        String Api = "GetAll";
 
+        new GetClass(this).execute("GetAll");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    public void parseResponse(String response) {
     }
 
     public void sendPostRequest(View View) {
-        new PostClass(this).execute();
+        String Api = "InfoGet";
+        String Id = "";
+        new PostClass(this).execute(Api, Id);
     }
 
     public void sendGetRequest(View View) {
-        new GetClass(this).execute();
+        String Api = "InfoGet";
+        String Id = "1001-1201-1221";
+        new GetClass(this).execute(Api, Id);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.numetriclabz.sendrequests/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.numetriclabz.sendrequests/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
     private class PostClass extends AsyncTask<String, Void, Void> {
 
         private final Context context;
 
-        public PostClass(Context c){
+        public PostClass(Context c) {
 
             this.context = c;
 //            this.error = status;
 //            this.type = t;
         }
 
-        protected void onPreExecute(){
-            progress= new ProgressDialog(this.context);
+        protected void onPreExecute() {
+            progress = new ProgressDialog(this.context);
             progress.setMessage("Loading");
             progress.show();
         }
@@ -70,7 +140,7 @@ public class MainActivity extends Activity {
                 final TextView outputView = (TextView) findViewById(R.id.showOutput);
                 URL url = new URL("http://people-count.azurewebsites.net");
 
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 String urlParameters = "fizz=buzz";
                 connection.setRequestMethod("POST");
                 connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
@@ -88,13 +158,13 @@ public class MainActivity extends Activity {
 
                 final StringBuilder output = new StringBuilder("Request URL " + url);
                 output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
-                output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
-                output.append(System.getProperty("line.separator")  + "Type " + "POST");
+                output.append(System.getProperty("line.separator") + "Response Code " + responseCode);
+                output.append(System.getProperty("line.separator") + "Type " + "POST");
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line = "";
                 StringBuilder responseOutput = new StringBuilder();
                 System.out.println("output===============" + br);
-                while((line = br.readLine()) != null ) {
+                while ((line = br.readLine()) != null) {
                     responseOutput.append(line);
                 }
                 br.close();
@@ -112,10 +182,8 @@ public class MainActivity extends Activity {
 
 
             } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             return null;
@@ -131,13 +199,13 @@ public class MainActivity extends Activity {
 
         private final Context context;
 
-        public GetClass(Context c){
+        public GetClass(Context c) {
             this.context = c;
         }
 
 
-        protected void onPreExecute(){
-            progress= new ProgressDialog(this.context);
+        protected void onPreExecute() {
+            progress = new ProgressDialog(this.context);
             progress.setMessage("Loading");
             progress.show();
         }
@@ -145,11 +213,18 @@ public class MainActivity extends Activity {
         @Override
         protected Void doInBackground(String... params) {
             try {
-
+                if (params.length < 1) {
+                    return null;
+                }
                 final TextView outputView = (TextView) findViewById(R.id.showOutput);
-                URL url = new URL("http://people-count.azurewebsites.net/api/InfoGet/");
-
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                String url_string = "http://people-count.azurewebsites.net/api/";
+                if (params.length > 0) {
+                    for (String component : params) {
+                        url_string = url_string + component + "/";
+                    }
+                }
+                URL url = new URL(url_string);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 String urlParameters = "fizz=buzz";
                 connection.setRequestMethod("GET");
                 connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
@@ -163,15 +238,19 @@ public class MainActivity extends Activity {
 
                 final StringBuilder output = new StringBuilder("Request URL " + url);
                 //output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
-                output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
-                output.append(System.getProperty("line.separator")  + "Type " + "GET");
+                output.append(System.getProperty("line.separator") + "Response Code " + responseCode);
+                output.append(System.getProperty("line.separator") + "Type " + "GET");
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line = "";
                 final StringBuilder responseOutput = new StringBuilder();
                 System.out.println("output===============" + br);
                 Log.d("Resopnse", "" + br);
-                while((line = br.readLine()) != null ) {
+                while ((line = br.readLine()) != null) {
                     responseOutput.append(line);
+                }
+                final JSONObject[] jsonObj = new JSONObject[1];
+                if(params[0].equals("GetAll")) {
+                    jsonObj[0] = new JSONObject(responseOutput.toString());
                 }
                 br.close();
 
@@ -181,12 +260,28 @@ public class MainActivity extends Activity {
                     public void handleMessage(Message message) {
                         // This is where you do your work in the UI thread.
                         // Your worker tells you in the message what to do.
+                        LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        if(jsonObj[0]==null){
+                            Toast toast = Toast.makeText(getApplicationContext(), responseOutput.toString(), Toast.LENGTH_LONG);
+                            LinearLayout toastLayout = (LinearLayout) toast.getView();
+                            TextView toastTV = (TextView) toastLayout.getChildAt(0);
+                            toastTV.setTextSize(25);
+                            toast.show();
+                            return;
+                        }
+                        for (int i = 0; i < jsonObj[0].length(); i++) {
+                            try {
+                                Button b = new Button(MainActivity.activity);
+                                b.setText(jsonObj[0].get(jsonObj[0].names().getString(i)).toString());
+                                ll.gravity = Gravity.CENTER;
+                                b.setLayoutParams(ll);
+                                ((LinearLayout) findViewById(R.id.myLayout)).addView(b);
+                            }
+                            catch(Exception e){
+                                e.printStackTrace();
+                            }
+                        }
 
-                        Toast toast = Toast.makeText(getApplicationContext(),responseOutput.toString(), Toast.LENGTH_LONG);
-                        LinearLayout toastLayout = (LinearLayout) toast.getView();
-                        TextView toastTV = (TextView) toastLayout.getChildAt(0);
-                        toastTV.setTextSize(50);
-                        toast.show();
                     }
                 };
                 Message msg = mHandler.obtainMessage();
@@ -201,12 +296,11 @@ public class MainActivity extends Activity {
                     }
                 });
 
-
             } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
             return null;
